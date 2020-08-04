11078f0e6a0586fd369d28939d1dcb5972dd8e38
ashirokova <68733125+ashirokova@users.noreply.github.com>
2020-08-04 10:58:42 +0300

    Add files via upload

---
+++ Task4Interface/Bike.java
@@ -0,0 +1,17 @@
+package Task4Interface;
+
+public class Bike extends Vehicle implements Rideable, Driveable {
+    public Bike(String name, String color) {
+        super(name, color);
+    }
+
+    public void drive() {
+
+    }
+
+    public void ride() {
+
+    }
+}
+
+
